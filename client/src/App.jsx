3cf23187dffa2956c5706e0c34089de5d2abf6c3
DiosOne <domaone83@proton.me>
2025-11-10 20:26:57 +1000

    removed cardformpage.jsx as no longer needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import CardFormPage from "./pages/CardFormPage";
 
 export default function App() {
   return (
