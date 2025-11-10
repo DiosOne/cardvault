94d3f9dba271ae8d38d2876aac0c77ab6c7225e1
DiosOne <domaone83@proton.me>
2025-11-10 17:36:29 +1000

    added user and password local temp storage and deletion, changed main to suit changes

---
+++ client/src/context/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(localStorage.getItem("token"));
+
+    useEffect(() => {
+        if (token) {
+            const savedUser= JSON.parse(localStorage.getItem("user"));
+            setUser(savedUser);
+        }
+    }, [token]);
+
+    const login= (data) => {
+        setUser(data.user);
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+    };
+
+    const logout= () => {
+        setUser(null);
+        setToken(null);
+        localStorage.clear();
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, token, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
