e62585dc8d5a17cfc7509656ffe5d0cd47b09fd6
DiosOne <domaone83@proton.me>
2025-11-10 23:35:52 +1000

    added navbar styling and changed layout of app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 export default function App() {
+  const {user}= useContext(AuthContext);
+
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link> |
-        <Link to="/dashboard"></Link> |
-        <Link to="/add">Add Card</Link>
-      </nav>
+      <Navbar/>
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add" element={<CardFormPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* protected route */}
+        <Route path="/dashboard" element={user? <Dashboard/> : Login}/>
       </Routes>
     </Router>
   );
