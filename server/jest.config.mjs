96db5d643b20f38166805a886d0d869421139ec4
DiosOne <domaone83@proton.me>
2026-01-31 00:17:29 +1000

    fix(server): enable jest esm tests

--- server/jest.config.mjs
+++ server/jest.config.mjs
@@ -1,7 +1,5 @@
 export default {
     testEnvironment: "node",
-    transform: {}, //disable babel transform
-    // moduleNameMapper: {
-    //     "^(\\.{1,2}/.*)\\.js$": "$1", //fix for ESM inports
-    // },
+    // extensionsToTreatAsEsm: ['.js'],
+    transform: {},
 };
