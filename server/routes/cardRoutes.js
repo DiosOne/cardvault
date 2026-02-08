7eb4ec58b159b63991541da78146cf984d2e2f1d
TheOmegaFett <shanemiller@iinet.net.au>
2026-02-09 06:58:03 +0800

    Add rate limiting to card routes

--- server/routes/cardRoutes.js
+++ server/routes/cardRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import { createRateLimiter } from "../middleware/rateLimit.js";
 import {
   getUserCards,
   addCard,
@@ -9,9 +10,10 @@ import {
 } from "../controllers/cardController.js";
 
 const router= express.Router();
+const readLimiter= createRateLimiter({windowMs: 60_000, max: 60});
 
-router.get("/public", getPublicCards);
-router.get("/", verifyToken, getUserCards);
+router.get("/public", readLimiter, getPublicCards);
+router.get("/", verifyToken, readLimiter, getUserCards);
 router.post("/", verifyToken, addCard);
 router.patch("/:id", verifyToken, updateCard);
 router.delete("/:id", verifyToken, deleteCard);
